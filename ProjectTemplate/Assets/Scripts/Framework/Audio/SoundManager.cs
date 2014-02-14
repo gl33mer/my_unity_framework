21c4dafc4f10d16bf30b1129da3946be2816302b
Taras Leskiv <tleskiv@nravo.com>
2014-02-14 13:55:18 +0200

    Initial commit.

---
+++ ProjectTemplate/Assets/Scripts/Framework/Audio/SoundManager.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using Com.Nravo.Framework;
+
+namespace Com.Nravo.Framework
+{
+    public class SoundManager : MonoSingleton<SoundManager>
+    {
+        [Range(0.0f, 1.0f)] public float soundVolume;
+        [Range(0.0f, 1.0f)] public float musicVolume;
+
+        public AudioClip backgroundMusic;
+        public AudioClip planeMusic;
+
+        public bool SoundEnabled { set; get; }
+        public bool MusicEnabled { set; get; }
+
+        private AudioSource _backgroundMusicSource;
+
+        #region init
+
+        public override void Init()
+        {
+            SoundEnabled = true;
+            MusicEnabled = true;
+            CreateBackgroundMusicSource();
+            if (MusicEnabled) { TurnMusicOn(); }
+        }
+
+        private void CreateBackgroundMusicSource()
+        {
+            GameObject go = new GameObject("Background Music");
+            go.transform.parent = Camera.main.transform;
+
+            _backgroundMusicSource = go.AddComponent<AudioSource>();
+            _backgroundMusicSource.clip = backgroundMusic;
+            _backgroundMusicSource.loop = true;
+            _backgroundMusicSource.volume = musicVolume;
+        }
+
+        #endregion
+
+        #region music
+
+        private void TurnMusicOff()
+        {
+            if (_backgroundMusicSource.isPlaying)
+            {
+                _backgroundMusicSource.Pause();
+            }
+            MusicEnabled = false;
+        }
+
+        private void TurnMusicOn()
+        {
+            _backgroundMusicSource.Play();
+            MusicEnabled = true;
+        }
+
+        #endregion
+
+        #region sound
+
+        public AudioSource Play(AudioClip clip)
+        {
+            return Play(clip, Camera.main.transform, soundVolume, 1.0f);
+        }
+
+        public AudioSource Play(AudioClip clip, Transform emitter)
+        {
+            return Play(clip, emitter, soundVolume, 1.0f);
+        }
+
+        public AudioSource Play(AudioClip clip, Transform emitter, float volume)
+        {
+            return Play(clip, emitter, volume, 1.0f);
+        }
+
+        public AudioSource Play(AudioClip clip, Transform emitter, float volume, float pitch)
+        {
+            GameObject go = new GameObject("Audio: " + clip.name);
+            go.transform.position = emitter.position;
+            go.transform.parent = emitter;
+
+            AudioSource source = go.AddComponent<AudioSource>();
+            source.clip = clip;
+            source.volume = volume;
+            source.pitch = pitch;
+            if (SoundEnabled) { source.Play(); }
+            Destroy(go, clip.length);
+            return source;
+        }
+            
+        // Play at point
+        public AudioSource Play(AudioClip clip, Vector3 point)
+        {
+            return Play(clip, point, soundVolume, 1.0f);
+        }
+
+        public AudioSource Play(AudioClip clip, Vector3 point, float volume)
+        {
+            return Play(clip, point, volume, 1.0f);
+        }
+
+        public AudioSource Play(AudioClip clip, Vector3 point, float volume, float pitch)
+        {
+            //Create an empty game object
+            GameObject go = new GameObject("Audio: " + clip.name);
+            go.transform.position = point;
+
+            //Create the source
+            AudioSource source = go.AddComponent<AudioSource>();
+            source.clip = clip;
+            source.volume = volume;
+            source.pitch = pitch;
+            if (SoundEnabled) { source.Play(); }
+            Destroy(go, clip.length);
+            return source;
+        }
+
+        #endregion
+    }
+}
---
+++ ProjectTemplate/Assets/Scripts/Framework/Serialization/EncryptedXmlSerializer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Serialization;
+
+using UnityEngine;
+
+namespace Com.Nravo.FlipTheBoard.PersistantStorage
+{
+		public static class EncryptedXmlSerializer
+	{
+		private static readonly string PrivateKey = SystemInfo.deviceUniqueIdentifier.Replace("-", string.Empty);
+
+		public static T Load<T>(string path)
+		{
+			var stream = new MemoryStream();
+			var writer = new StreamWriter(stream);
+			var reader = new StreamReader(path);
+			string decryptedData = DecryptData(reader.ReadToEnd());
+			writer.AutoFlush = true;
+			writer.WriteLine(decryptedData);
+			stream.Flush();
+			stream.Position = 0;
+			var serializer = new XmlSerializer(typeof(T));
+			T result = (T) serializer.Deserialize(stream);
+			reader.Close();
+			writer.Close();
+			return result;
+		}
+
+		public static void Save<T>(string path, object value)
+		{
+			var serializer = new XmlSerializer(typeof(T));
+			using (var stream = new MemoryStream())
+			{
+				serializer.Serialize(stream, value);
+				stream.Flush();
+				stream.Position = 0;
+				string sr = new StreamReader(stream).ReadToEnd();
+				var fileStream = new FileStream(path, FileMode.Create);
+				var streamWriter = new StreamWriter(fileStream);
+				streamWriter.WriteLine(EncryptData(sr));
+				streamWriter.Close();
+				fileStream.Close();
+			}
+		}
+
+		private static string EncryptData(string toEncrypt)
+		{
+			byte[] toEncryptArray = Encoding.UTF8.GetBytes(toEncrypt); 
+			RijndaelManaged rDel = CreateRijndaelManaged();
+			ICryptoTransform cTransform = rDel.CreateEncryptor(); 
+			byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length); 
+			return Convert.ToBase64String(resultArray, 0, resultArray.Length); 
+		}
+
+		private static string DecryptData(string toDecrypt)
+		{ 
+			byte[] toEncryptArray = Convert.FromBase64String(toDecrypt);
+			RijndaelManaged rDel = CreateRijndaelManaged();
+			ICryptoTransform cTransform = rDel.CreateDecryptor(); 
+			byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length); 
+			return Encoding.UTF8.GetString(resultArray); 
+		}
+
+		private static RijndaelManaged CreateRijndaelManaged()
+		{
+			byte[] keyArray = Encoding.UTF8.GetBytes(PrivateKey); 
+			var result = new RijndaelManaged();
+
+			var newKeysArray = new byte[16];
+			Array.Copy(keyArray, 0, newKeysArray, 0, 16);
+
+			result.Key = newKeysArray;
+			result.Mode = CipherMode.ECB;
+			result.Padding = PaddingMode.PKCS7;
+			return result;
+		}
+	}
+}
