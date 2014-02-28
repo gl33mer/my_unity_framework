8946ad862cb770079b85bd17ebf3c72c841f4a54
Taras Leskiv <tleskiv@nravo.com>
2014-02-28 13:27:06 +0200

    Added custom list from http://catlikecoding.com/unity/tutorials/editor/custom-list/

---
+++ ProjectTemplate/Assets/AdditionalFramework/Examples/CatlikeCodingCustomListEditor/CustomListDemo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CustomListDemo : MonoBehaviour
+{
+    public int[] integers;
+
+    public Vector3[] vectors;
+
+    public string[] strings;
+
+    public Transform[] objects;
+
+    public int notAList;
+}
---
+++ ProjectTemplate/Assets/AdditionalFramework/Examples/CatlikeCodingCustomListEditor/Editor/ListTesterInspector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CustomListDemo)), CanEditMultipleObjects]
+public class ListTesterInspector : Editor {
+
+	public override void OnInspectorGUI () {
+		serializedObject.Update();
+		EditorList.Show(serializedObject.FindProperty("integers"), EditorListOption.ListSize);
+		EditorList.Show(serializedObject.FindProperty("vectors"));
+        EditorList.Show(serializedObject.FindProperty("strings"), EditorListOption.Buttons);
+		EditorList.Show(serializedObject.FindProperty("objects"), EditorListOption.ListLabel | EditorListOption.Buttons);
+		EditorList.Show(serializedObject.FindProperty("notAList"));
+		serializedObject.ApplyModifiedProperties();
+	}
+}
