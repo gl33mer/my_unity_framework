db2fba4d9212a062ca5fb887dab98a59f2aca35d
Taras Leskiv <tleskiv@nravo.com>
2014-03-12 16:51:40 +0200

    Extension methods added.

---
+++ ProjectTemplate/Assets/AdditionalFramework/Utils/ExtensionMethods/CollectionExtesionMethods.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+public static class CollectionExtesionMethods
+{
+    public static bool IsEmpty(this ICollection collection)
+    {
+        return collection.Count == 0;
+    }
+}
