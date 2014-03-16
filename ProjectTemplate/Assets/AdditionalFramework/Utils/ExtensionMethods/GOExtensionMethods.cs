dcb655913950d3fdd5c0566381bdf604e78ab4ed
Taras Leskiv <tleskiv@nravo.com>
2014-03-16 13:59:21 +0200

    DestroyAllChildren method added.

---
+++ ProjectTemplate/Assets/AdditionalFramework/Utils/ExtensionMethods/GOExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class GOExtensionMethods
+{
+    public static void DestroyAllChildren(this Transform parent)
+    {
+        foreach (Transform childTransform in parent.transform)
+        {
+            GameObject.Destroy(childTransform.gameObject);
+        }
+    }
+
+    public static void DestroyAllChildren(this GameObject parent)
+    {
+        foreach (Transform childTransform in parent.transform)
+        {
+            GameObject.Destroy(childTransform.gameObject);
+        }
+    }
+}
